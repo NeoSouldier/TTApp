70614dc21afd3722893f5d0927a51868d68830e8
Arthur Fox <vreel@vreel.io>
2019-02-08 20:13:26 -0200

    Added a VideoPlayer component and hooked it up
    Simple change here, I just added a Video Player component and hooked up a video from within the project.
    
    I was very happy to find that it re-adjusted very well in all the different resolutions. Next step is to test it out with a url from youtube, I think that's the only unknown in this project, everything else will be polish and building the logic.

---
+++ TT-Unity/Assets/Scripts/AppDirector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppDirector : MonoBehaviour 
+{
+
+    // **************************
+    // Member Variables
+    // **************************
+
+    public enum AppState
+    {
+        kStartScreen   // Start screen
+    }
+        
+    //[SerializeField] private GameObject m_mainMenu;
+
+    private AppState m_appState;
+
+    // **************************
+    // Public functions
+    // **************************
+
+	public void Start () 
+    {
+        m_appState = AppState.kStartScreen;
+	}
+	
+	public void Update () 
+    {
+        // empty
+	} 
+
+    // **************************
+    // Private/Helper functions
+    // **************************
+        
+    private void Empty()
+    {
+        //
+    }
+}
