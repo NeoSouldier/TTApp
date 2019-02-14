451783563c38e43814176f25d30fa300b4f530f6
Arthur Fox <vreel@vreel.io>
2019-02-14 16:55:58 -0200

    Made a basic initial flow for the actual app
    Setup a basic flow where you have a Start screen, that you click through to the Main Menu. From there you have the option of Beginner, Intermediate, Advanced videos and when you select one of them you can select the first video (which just leads to the same video as before). This little flow all happens now in the AppDirector though and the scene is a lot cleaner. Still have an odd issue about ordering with the loading screen, as it loads a behind my scene.

--- TT-Unity/Assets/Scripts/AppDirector.cs
+++ TT-Unity/Assets/Scripts/AppDirector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class AppDirector : MonoBehaviour 
 {
@@ -11,12 +12,26 @@ public class AppDirector : MonoBehaviour
 
     public enum AppState
     {
-        kStartScreen   // Start screen
+        kStartScreen = 0,   // Start screen
+        kMainMenu = 1,      // Main Menu
+        kBeginner = 2,      // Beginner
+        kIntermediate = 3,  // Intermediate
+        kAdvanced = 4,      // Advanced
+        kMax = 5
     }
-        
-    //[SerializeField] private GameObject m_mainMenu;
+
+    [SerializeField] private GameObject m_canvasTopLevel;
+    [SerializeField] private GameObject m_startScreen;
+    [SerializeField] private GameObject m_mainMenu;
+    [SerializeField] private GameObject m_beginner;
+    [SerializeField] private GameObject m_intermediate;
+    [SerializeField] private GameObject m_advanced;
+    [SerializeField] private YoutubePlayer m_youtubePlayer;
+    [SerializeField] private VideoPlayer m_videoPlayer;
 
     private AppState m_appState;
+    private GameObject[] screens;
+    private int m_currVideoIndex;
 
     // **************************
     // Public functions
@@ -24,7 +39,8 @@ public class AppDirector : MonoBehaviour
 
 	public void Start () 
     {
-        m_appState = AppState.kStartScreen;
+        Init();
+        SetState((int)AppState.kStartScreen);
 	}
 	
 	public void Update () 
@@ -32,12 +48,70 @@ public class AppDirector : MonoBehaviour
         // empty
 	} 
 
+    public void SetTEST(AppState appState)
+    {
+        //
+    }
+
+    public void SetState(int state)
+    {
+        SetStateInternal((AppState)state);
+    }
+
+    public void PlayVideo(int videoIndex)
+    {
+        PlayVideoInternal(videoIndex);
+    }
+
+    public void VideoStarted()
+    {
+        m_canvasTopLevel.SetActive(false);
+        m_videoPlayer.targetCameraAlpha = 1;
+    }
+
+    public void VideoFinished()
+    {
+        m_canvasTopLevel.SetActive(true);
+        m_videoPlayer.targetCameraAlpha = 0;
+    }
+
     // **************************
     // Private/Helper functions
     // **************************
-        
-    private void Empty()
+
+    private void Init()
     {
-        //
+        screens = new GameObject[(int)AppState.kMax];
+        screens[(int)AppState.kStartScreen] = m_startScreen;
+        screens[(int)AppState.kMainMenu] = m_mainMenu;
+        screens[(int)AppState.kBeginner] = m_beginner;
+        screens[(int)AppState.kIntermediate] = m_intermediate;
+        screens[(int)AppState.kAdvanced] = m_advanced;
+
+        m_currVideoIndex = -1;
+    }
+
+    private void SetStateInternal(AppState appState)
+    {
+        m_appState = appState;
+        for (int i = 0; i < (int)AppState.kMax; i++ )
+        {
+            bool isNewState = i == (int)appState;
+            screens[i].SetActive(isNewState);
+        }
+    }
+
+    private void PlayVideoInternal(int videoIndex)
+    {
+        bool isNewVideo = m_currVideoIndex != videoIndex;
+        if (isNewVideo) // load new video
+        {
+            string videoURL = "https://www.youtube.com/watch?v=y8Kyi0WNg40";
+            m_youtubePlayer.LoadYoutubeVideo(videoURL);
+        }
+        else
+        {
+            m_youtubePlayer.PlayButton();
+        }
     }
 }
